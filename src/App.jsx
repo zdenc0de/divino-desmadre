a133ba305153277bae61f99b2444e9b6e693a731
Zdenko Abarca <eabarcac004@alumno.uaemex.mx>
2025-08-31 23:15:48 -0600

    Routers and HomePage default

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { MyRoutes } from './routers/router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 function App() {
-  const [count, setCount] = useState(0)
   const queryClient = new QueryClient()
 
   return (
     <QueryClientProvider 
       client={queryClient}>
+        <MyRoutes />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+export const HomePage = () => {
+    return (
+        <div
+        className="h-screen bg-amber-300 text-black">
+               <span>
+                   HomePage
+               </span>
+        </div>
+    )
+}
