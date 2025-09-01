55ffd9ad2cb66b5a882468946066292e0db3a2cc
Zdenko Abarca <eabarcac004@alumno.uaemex.mx>
2025-08-31 23:39:11 -0600

    Global Layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,10 +3,16 @@ import { Sidebar } from "../components/SideBar/Sidebar"
 
 export const MainLayout = () => {
     return (
-        <div
-        className="h-screen bg-amber-300 text-black">
-               <Sidebar />
-               <Outlet />
-        </div>
+        <main
+        className="flex justify-center h-screen overflow-hidden bg-white dark:bg-bg-dark text-black dark:text-white transition-colors duration-300">
+            <section
+            className="flex w-full max-w-[1300px] h-full">
+                <Sidebar />
+                <section className="flex-1 px-4 overflow-y-auto h-full">
+                    <Outlet />
+                </section>
+            </section>
+            
+        </main>
     )
 }
