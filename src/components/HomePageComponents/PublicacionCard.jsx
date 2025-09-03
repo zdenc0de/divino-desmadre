67e5d899d685562aa00a12af981a3933cd38a692
Zdenko Abarca <eabarcac004@alumno.uaemex.mx>
2025-09-03 14:52:54 -0600

    Publication footer

--- src/components/HomePageComponents/PublicacionCard.jsx
+++ src/components/HomePageComponents/PublicacionCard.jsx
@@ -37,7 +37,26 @@ export const PublicacionCard = () => {
                        saludame porras
                    </p>
                    <div>
-                    <PostImageFrame src={"https://tse1.mm.bing.net/th/id/OIP.GKq_s07K8lmVurS7GwVAvAHaJj?cb=thfc1&w=735&h=948&rs=1&pid=ImgDetMain&o=7&rm=3"} />
+                    <PostImageFrame 
+                    src={"https://tse1.mm.bing.net/th/id/OIP.GKq_s07K8lmVurS7GwVAvAHaJj?cb=thfc1&w=735&h=948&rs=1&pid=ImgDetMain&o=7&rm=3"} />
+                   </div>
+                   <div
+                   className="flex justify-between mt-4">
+                        <button>
+                            <Icon
+                            icon = {"mdi:heart-outline"} 
+                            className="text-3xl p-1 rounded-full text-gray-400 hover:bg-[rgba(78, 184, 233, 0.5)] cursor-pointer"/>
+                        </button>
+                        <button
+                        className="flex items-center gap-2 cursor-pointer">
+                            <Icon
+                            icon = {"mdi:comment-outline"} 
+                            className="text-3xl p-1 rounded-full text-gray-400 cursor-pointer"/>
+                            <span
+                            className="text-xs md:text-sm text-gray-400">
+                                Comentar
+                            </span>
+                        </button>
                    </div>
                </div>
         </div>
