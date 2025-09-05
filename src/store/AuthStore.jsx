a56a56cba40292a08f0b01c6e2e495882644ead0
Zdenko Abarca <eabarcac004@alumno.uaemex.mx>
2025-09-04 21:30:55 -0600

    useSubscription

--- src/store/AuthStore.jsx
+++ src/store/AuthStore.jsx
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { supabase } from "../supabase/supabase.config";
+import { data } from "react-router-dom";
 
 export const useAuthStore = create((set) => ({
     // Estado
@@ -23,3 +24,25 @@ export const useAuthStore = create((set) => ({
         return data.user;
     }
 }));
+
+export const useSubscription = create((set) => {
+    // Inicia el estado
+    const store = {
+        user:null, 
+        setUser:(user) => set({user}),
+    }
+    // Listener que se ejecuta una vez cuando se importa el store
+    supabase.auth.getSession().then(({data:{session}}) => {
+        if (session?.user) {
+            set({user:session.user})
+        }
+    })
+    supabase.auth.onAuthStateChange((event, session) => {
+        if (session?.user) {
+            set({user:session.user})
+        } else {
+            set({user:null})
+        }
+    })
+    return store; 
+})
