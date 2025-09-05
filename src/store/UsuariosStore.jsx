319f3f2cc206c1479d96f05271971008d4b87363
Zdenko Abarca <eabarcac004@alumno.uaemex.mx>
2025-09-05 15:28:04 -0600

    Show auth users

---
+++ src/store/UsuariosStore.jsx
@@ -0,0 +1,18 @@
+import { create } from "zustand";
+import { supabase } from "../supabase/supabase.config";
+
+const tabla = "usuarios";
+
+export const useUsuariosStore = create((set) => ({
+	mostrarUsuarioAuth: async (p) => {
+        const {data, error} = await supabase
+        .from(tabla)
+        .select()
+        .eq("id_auth", p.id_auth)
+        .maybeSingle(); 
+        if (error) {
+            throw new Error(error.message)
+        }
+        return data;
+    }, 
+}));
