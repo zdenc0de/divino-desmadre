7674504009f16e3e71ea29ad06fd774e02e601fc
Zdenko Abarca <eabarcac004@alumno.uaemex.mx>
2025-09-08 10:29:20 -0600

    Enlazando emoji picker

--- src/components/Forms/FormPost.jsx
+++ src/components/Forms/FormPost.jsx
@@ -1,11 +1,24 @@
 import { BtnClose } from "../ui/buttons/BtnClose"
 import { useUsuariosStore } from "../../store/UsuariosStore"
-import { useState } from "react";
+import { useRef, useState } from "react";
 import EmojiPicker from "emoji-picker-react";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 export const FormPost = () => {
+
     const { dataUsuarioAuth } = useUsuariosStore()
     const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+    const textarea = useRef(null);
+    const [postText, setPostText] = useState("");
+    const addEmoji = (emojiData) => {
+        const emojiChar = emojiData.emoji
+        const textarea = textarea.current;
+        const start = textarea.selectionStart;
+        const end = textarea.selectionEnd;
+        
+
+    }
+
     return (
         <main className="fixed z-50 flex items-center justify-center inset-0">
             {/* Backdrop que cubre toda la pantalla */}
@@ -43,13 +56,33 @@ export const FormPost = () => {
                         <div
                         className="relative">
                             <textarea
+                            ref={textarea}
                             placeholder="¿Qué estás pensando?"
                             className="w-full placeholder-gray-500 outline-none"
                             />
-                            {
-                                showEmojiPicker && 
-                                <div><EmojiPicker /></div>
+                            { showEmojiPicker && 
+                                (<div
+                                className="absolute top-10 left-10 mt-2">
+                                    <EmojiPicker 
+                                    onEmojiClick={addEmoji}
+                                    theme="auto" />
+                                </div>)
                             }
+                            <div
+                            className="mt-4 flex items-center justify-between">
+                                <button
+                                type="submit"
+                                className="py-2 px-4 rounded-lg font-medium bg-primary text-white cursor-pointer">
+                                    Publicar
+                                </button>
+                                <button
+                                onClick={() => setShowEmojiPicker(!showEmojiPicker)}
+                                type="button"
+                                className="p-1 text-black/50 dark:text-white/50 hover:bg-gray-700 rounded-full cursor-pointer hover:text-white hover:dark:text-white">
+                                    <Icon icon="mdi:emoticon-outline"
+                                    className="text-2xl" />
+                                </button>
+                            </div>
                         </div>
                     </form>
                 </main>
