6a38cd1d6daedcf81e9741536b1a4b75cacb6e86
Zdenko Abarca <eabarcac004@alumno.uaemex.mx>
2025-09-14 09:53:48 -0600

    Comment's emojis fixed

--- src/components/HomePageComponents/ComentarioModal.jsx
+++ src/components/HomePageComponents/ComentarioModal.jsx
@@ -1,11 +1,56 @@
 import { Icon } from "@iconify/react/dist/iconify.js"
 import { BtnClose } from "../ui/buttons/BtnClose"
 import { useInsertarComentarioMutate } from "../../stack/ComentariosStack"
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import EmojiPicker from "emoji-picker-react";
 
 export const ComentarioModal = ({item, onClose}) => {
     const [comentario, setComentario] = useState("");
     const { mutate: comentarioMutate } = useInsertarComentarioMutate({comentario: comentario, setComentario: setComentario});
+    const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+    const pickerRef = useRef(null);
+    const textComentarioRef = useRef(null);
+    const emojiButtonRef = useRef(null);
+
+const addEmoji = (emojiData) => {
+    const emojiChar = emojiData.emoji;
+    const textarea = textComentarioRef.current;
+    if (!textarea) return;
+
+    const start = textarea.selectionStart;
+    const end = textarea.selectionEnd;
+    const originalText = comentario;
+
+    const newText =
+      originalText.substring(0, start) +
+      emojiChar +
+      originalText.substring(end);
+
+    setComentario(newText);
+
+    // Reubicar el cursor justo después del emoji
+    setTimeout(() => {
+      textarea.focus();
+      textarea.selectionStart = textarea.selectionEnd = start + emojiChar.length;
+    }, 0);
+
+    // Cerrar el picker después de seleccionar un emoji
+    setShowEmojiPicker(false);
+  };
+
+  useEffect(() => {
+      const handleClickOutside = (e) => {
+        if (pickerRef.current && !pickerRef.current.contains(e.target) &&
+            emojiButtonRef.current && !emojiButtonRef.current.contains(e.target)) {
+          setShowEmojiPicker(false);
+        }
+      };
+  
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
+
+
     return (
         <main
         className="fixed inset-0 z-100 bg-black/50 backdrop-blur-sm flex items-center justify-center p-4">
@@ -46,12 +91,20 @@ export const ComentarioModal = ({item, onClose}) => {
                     src="https://locosxlosjuegos.com/wp-content/uploads/2022/09/sjsjs.jpg"
                     className="w-10 h-10 rounded-full object-cover" alt="Avatar"/>
                             <input 
+                            ref={textComentarioRef}
                             className="flex-1 bg-gray-100 dark:bg-bg-dark text-sm rounded-2xl px-4 py-2 focus:outline-none resize-none"
                             placeholder="Escribe un comentario..."
                             value={comentario}
                             onChange={(e) => setComentario(e.target.value)}
                             />
+                             {showEmojiPicker && (
+                                            <div className="absolute top-10 left-10 mt-2 z-50" ref={pickerRef}>
+                                              <EmojiPicker onEmojiClick={addEmoji} theme="auto" searchDisabled />
+                                            </div>
+                                          )}
                             <button
+                                ref={emojiButtonRef}
+                                onClick={() => setShowEmojiPicker(!showEmojiPicker)}
                             className="text-gray-500 hover:text-gray-700 relative">
                                 <Icon 
                                 icon="mdi:emoticon-outline" 
